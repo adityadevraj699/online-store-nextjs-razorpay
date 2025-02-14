7dd888d209b37ab353fd9011c4a344a98319dbec
Aditya kumar <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2025-02-15 05:02:38 +0530

    Implement NextAuth for authentication with credentials provider

---
+++ next-auth.d.ts
@@ -0,0 +1,16 @@
+import { DefaultSession } from "next-auth";
+
+
+declare module "next-auth"{
+    interface Session {
+        user: {
+            role: string;
+            id: string;
+          } & DefaultSession["user"];
+    }
+
+    interface User {
+        role: string;
+    }
+}
+
